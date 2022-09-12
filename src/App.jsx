3166361aaa428060236592e003dc622d9d4ef6b6
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-09-12 12:30:46 +0200

    Update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div>Luma Hotel Alarm App</div>
-  )
-}
+  return <div>Luma Hotel Alarm App</div>;
+};
 
-export default App
+export default App;
