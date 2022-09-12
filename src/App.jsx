8c52f7f7e5c891d865ec2477f01671181df3600b
Mai Ly T <88801003+MaiLyT@users.noreply.github.com>
2022-09-12 20:22:51 +0200

    Logga in och ut funktion

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { PageLayout } from './components/PageLayout';
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from '@azure/msal-react';
 
 const App = () => {
   return (
-    <div>Luma Hotel Alarm App</div>
+    <PageLayout>
+      <AuthenticatedTemplate>
+        <div>Luma Climate App</div>
+        <p>Only logged in user can read this</p>
+      </AuthenticatedTemplate>
+      <UnauthenticatedTemplate>
+        <p> You are not signed in! Please do sign in to use Luma Climate App</p>
+      </UnauthenticatedTemplate>
+    </PageLayout>
+    
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import App from "./App";
 import ReactDOM from "react-dom/client";
 import React from "react";
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from '../authentication/authConfig';
+
+const msalInstance = new PublicClientApplication(msalConfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
   </React.StrictMode>
 );
