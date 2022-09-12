f374a40037739cced94c54e92e1949dd64d84857
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-09-12 12:06:45 +0200

    Remove unused import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import App from "./App";
 import ReactDOM from "react-dom/client";
 import React from "react";
-import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
