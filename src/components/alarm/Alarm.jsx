eab850f64e829dee693cd35dcccd62ceee61eaad
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-09-13 12:12:52 +0200

    Create Alarm component

---
+++ src/components/alarm/Alarm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Alarm.css'
+
+const Alarm = () => {
+  return (
+    <div className='alarm'>
+        <div>Alarm</div>
+    </div>
+  )
+}
+
+export default Alarm
