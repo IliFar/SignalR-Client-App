4fb006131ff7b8d17d172c6ef83dd43a4aee0d72
Mai Ly T <88801003+MaiLyT@users.noreply.github.com>
2022-09-13 17:48:13 +0200

    Flytta Header, Search, Alarm, Menu till PageLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
 import React from 'react';
 import { PageLayout } from './components/PageLayout';
 import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from '@azure/msal-react';
-import Alarm from "./components/alarm/Alarm";
-import Header from "./components/header/Header";
-import Menu from "./components/menu/Menu";
-import Search from "./components/search/Search";
+
 import "./App.css";
 
 const App = () => {
   return (
-     <div className="container">
+    <div className="container">
       <PageLayout>
         <AuthenticatedTemplate>
           <p>Only logged in user can read this</p>
-          <Header />
-          <Search />
-          <Alarm />
-          <Menu />
+        
         </AuthenticatedTemplate>
         <UnauthenticatedTemplate>
           <p> You are not signed in! Please do sign in to use Luma Climate App</p>
         </UnauthenticatedTemplate>
       </PageLayout>
-     </div>
+    </div>
   );
-  )
+
 }
 
 export default App;
--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,27 +1,31 @@
 import React from "react";
-import { useIsAuthenticated, useMsal } from "@azure/msal-react";
+import { useIsAuthenticated, useMsal, AuthenticatedTemplate } from "@azure/msal-react";
 import { SignInButton } from "./SignInButton";
 import { SignOutButton } from "./SignOutButton";
-
+import Alarm from "./alarm/Alarm";
+import Header from "./header/Header";
+import Menu from "./menu/Menu";
+import Search from "./search/Search";
+import "../App.css";
 /**
  * Renders the navbar component with a sign-in button if a user is not authenticated
  */
-
-
 export const PageLayout = (props) => {
     const {accounts} = useMsal();
     const isAuthenticated = useIsAuthenticated();
-    const username = isAuthenticated? accounts[0].name : '';
+    const name = isAuthenticated? accounts[0].name : '';
     return (
         <>
-            <div className='navbar' bg="primary" variant="dark">
+            <Header />
+            <Search />
+            <Alarm />
+            <Menu />
+            <div className='menu' bg="primary" variant="dark">
                 <a className="navbar-brand" href="/">Home</a>
-                { isAuthenticated ? <span><SignOutButton />  Hi {username}!</span> : <SignInButton /> }
+                { isAuthenticated ? <span><SignOutButton />Hi {name}</span> : <SignInButton /> }
             </div>
-            <h5><center>Welcome to Luma Hotel</center></h5>
-            <br />
-            <br />
-            {props.children}
+        
+        
         </>
     );
 };
