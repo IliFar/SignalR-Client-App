027fe13291b3d4fd0f6fa238c85944105d7bc8da
Mai Ly T <88801003+MaiLyT@users.noreply.github.com>
2022-09-14 12:15:48 +0200

    hämta accessToken

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,31 +1,66 @@
-import React from "react";
-import { useIsAuthenticated, useMsal, AuthenticatedTemplate } from "@azure/msal-react";
-import { SignInButton } from "./SignInButton";
-import { SignOutButton } from "./SignOutButton";
+import React, {useState, useEffect} from "react";
+import { useIsAuthenticated, useMsal, AuthenticatedTemplate, UnauthenticatedTemplate } from "@azure/msal-react";
 import Alarm from "./alarm/Alarm";
 import Header from "./header/Header";
 import Menu from "./menu/Menu";
 import Search from "./search/Search";
 import "../App.css";
-/**
- * Renders the navbar component with a sign-in button if a user is not authenticated
- */
-export const PageLayout = (props) => {
-    const {accounts} = useMsal();
+import { loginRequest } from "../../authentication/authConfig";
+import {getBuildingInfo, getAllDevices } from "../data/apis";
+
+
+export function PageLayout (props){
     const isAuthenticated = useIsAuthenticated();
-    const name = isAuthenticated? accounts[0].name : '';
+    const { instance, accounts, inProgress } = useMsal();
+    const name = isAuthenticated? accounts[0].name : ' ';
+    const request = {
+        ...loginRequest,
+        account: accounts[0]
+    };
+    const [accessToken, setAccessToken] = useState(null);
+    // Silently acquires an access token which is then attached to a request for Microsoft Graph data
+    instance.acquireTokenSilent(request).then((response) => {
+    setAccessToken(response.accessToken);
+    }).catch((e) => {
+    instance.acquireTokenPopup(request).then((response) => {
+        setAccessToken(response.accessToken);
+    });
+}); 
+
+// const [bldInfo, setBldInfo] = useState(null);
+
+// useEffect( () => {
+//     async function fetchBldInfo(accessToken){
+//         const res = await getBuildingInfo(accessToken);
+//         setBldInfo(res);
+//         console.log(res);
+//     };
+//     fetchBldInfo(accessToken) 
+// }, [])
+// console.log('buildInfo', bldInfo);
+
+// const res = getBuildingInfo(accessToken);
+//         console.log('res', res);
+
     return (
         <>
-            <Header />
-            <Search />
-            <Alarm />
-            <Menu />
-            <div className='menu' bg="primary" variant="dark">
-                <a className="navbar-brand" href="/">Home</a>
-                { isAuthenticated ? <span><SignOutButton />Hi {name}</span> : <SignInButton /> }
-            </div>
+            {/* Signed and Unsigned user can see Menu and Header  */}
+            <Header name={name} isAuthenticated={isAuthenticated}/>
         
-        
+            {/* Only signed in user can see content in AuthenticatedTemplate */}
+            <AuthenticatedTemplate>
+                
+                <Alarm />
+                <Search />  
+            </AuthenticatedTemplate>
+
+            {/* Unsigned in user see content in UnauthenticatedTemplate */}
+            <UnauthenticatedTemplate>
+                <p> You are not signed in! Please do sign in to use Luma Climate App</p>
+            </UnauthenticatedTemplate>
+            
+            <Menu isAuthenticated={isAuthenticated}/>
+            
         </>
     );
 };
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 //import User from "../user/User";
 import "./Header.css";
+import { SignInButton } from "../SignInButton";
+import { SignOutButton } from "../SignOutButton";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <div className="header">
       <div className="logo">
@@ -12,8 +14,9 @@ const Header = () => {
           className="logo-img"
         />
       </div>
+      
       <div className="user-avatar">
-        <h4 className="username">Hi, User</h4>
+        <h4 className="username">Hi, {props.name}</h4>
         <img
           src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
           alt=""
