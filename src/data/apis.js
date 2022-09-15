deedb6bef8b692008dcdaf9ef40681fec97a2173
Mai Ly T <88801003+MaiLyT@users.noreply.github.com>
2022-09-15 11:18:48 +0200

    API funktion hämta byggnad och sensorer

--- src/data/apis.js
+++ src/data/apis.js
@@ -1,49 +1,22 @@
-// const axios = require('axios');
+const axios = require('axios');
 
 async function getBuildingInfo(token){
-
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", `Bearer ${token}`);
-
-    var requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-    };
-
-    await fetch("https://api.smarthut.se/BuildingInfo/GetMyBuilding", requestOptions)
-    .then(response => JSON.stringify(response.data))
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-//     axios({
-//         method: 'get',
-//         url: 'https://api.smarthut.se/BuildingInfo/GetMyBuilding',
-//         headers: { Authorization: `Bearer ${token}` }
-//     })
-//     .then(function (response) {
-//         console.log(response);
-//     return JSON.stringify(response.data);
-//     })
-//     .catch(function (error) {
-//     console.log(error);
-//     });
+    return axios({
+        method: 'get',
+        url: 'https://api.smarthut.se/BuildingInfo/GetMyBuilding',
+        headers: { Authorization: `Bearer ${token}` }
+    })
+    
 }
 
 
-// function getAllDevices(token, buildingId){
-//     axios({
-//         method: 'get',
-//         url: 'https://api.smarthut.se/DeviceInfo/GetBuildingDevices/'+ buildingId,
-//         headers: { Authorization: `Bearer ${token}` }
-//     })
-//     .then(function (response) {
-//         console.log(JSON.stringify(response.data));
-//     return JSON.stringify(response.data);
-//     })
-//     .catch(function (error) {
-//     console.log(error);
-// });
-// }
+function getAllDevices(token, buildingId){
+    return axios({
+        method: 'get',
+        url: 'https://api.smarthut.se/DeviceInfo/GetBuildingDevices/'+ buildingId,
+        headers: { Authorization: `Bearer ${token}` }
+    })
+}
 
 
 
